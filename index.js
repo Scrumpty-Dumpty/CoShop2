3eb9f9133607f08e4205f602a539984fd1a0a2e2
Amna Tasneem <amnaxtasneem@gmail.com>
2023-04-23 15:43:27 -0500

    Cleaned up registration code. Note: user profile upload is not stored in database.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // connect to the users database
 const { connectToDB } = require ('./client/assets/scripts/dbConnection.js');
 const User = require ('./client/models/User.js');
-const mongoose  = require('mongoose');
-
 
 connectToDB();
 
@@ -21,10 +19,6 @@ app.get("/test", function (request, response) {
   response.type("text/plain");
   response.send("Node.js and Express running on port=" + port);
 });
-
-
-
-//----------------------------
 
 const users = []
 app.post('/register', async (req, res) => {
@@ -45,61 +39,8 @@ const newUser = new User({
       await newUser.save();
 } catch (err){
   console.log('user not entered into database: ', err);  
-
   }
 })
 app.listen(port, function () {
   console.log("Server is running at http://localhost:3000/");
 });
-// let latestUser = users.slice(-1);
-// console.log(latestUser.name);
-// const newUser = new User({
-//   name: latestUser.name,
-//         email: latestUser.email,
-//         password: latestUser.password
-//     });
-//     console.log('newUser: ', newUser.name);
-     // Save the new user to the database
-    //  newUser.save();
-    
-
-
-
-// app.post('/register', async (req, res) => {
-//   try {
-//     const hashedPassword = await bcrypt.hash(req.body.pass1, 10)
-//     const newUser = new User({
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: hashedPassword
-//     })
-//     await newUser.save();
-// console.log('user created');  
-// console.log(newUser);
-// } catch(err) {
-//   console.error(err);  
-
-//   }
-// })
-
-//----------------------------
-
-// ROUTE TO HANDLE USER CREATION
-// app.post('/register', async (req, res) => {
-//   try {
-//     const { name, email, pass1 } = req.body;
-//     const password = await bcrypt.hash(pass1, 10);
-//     const newUser = new User({
-//       name,
-//       email,
-//       password
-//   });
-
-//     // Save the new user to the database
-//     await newUser.save();
-//     console.log('User added to database successfully!')
-//           } catch (err) {
-//             console.error(err); 
-//             console.log('User was not added to database.')
-//           }
-//  });
