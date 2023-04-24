b1f69c839064018817f5c64601e83ce55120e4ca
Amna Tasneem <amnaxtasneem@gmail.com>
2023-04-24 01:43:09 -0500

    Revert "User registration, + added to  database."

--- client/models/User.js
+++ client/models/User.js
@@ -22,3 +22,30 @@ const userSchema = new mongoose.Schema({
   const User = mongoose.model('User', userSchema); 
 
   module.exports = User;
+
+  // Connect to MongoDB
+// mongoose.connect('mongodb+srv://CoShopUser:6TvlGsCx9PGmwpNl@coshopcluster.iyjalfb.mongodb.net/CoShopDB?retryWrites=true&w=majority', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   });
+  
+//   // Create a new User document and save it to the database
+//   const newUser = new User({
+//     name: 'John Doe',
+//     email: 'johndoe@example.com',
+//     password: 'mypassword'
+//   });
+  
+//   newUser.save()
+//     .then(savedUser => {
+//       console.log('User document saved successfully:', savedUser);
+//       // Close the MongoDB connection
+//       mongoose.connection.close();
+//     })
+//     .catch(error => {
+//       console.error('Error saving User document:', error);
+//       // Close the MongoDB connection
+//       mongoose.connection.close();
+//     });
+
+
